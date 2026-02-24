e7f3a4ca47ba381c1068ac3ab5a10f5577656cc7
dvin-22 <dain5835@gmail.com>
2026-02-24 19:34:00 +0900

    [feat] 영속성 리소스 구현

---
+++ modules/storage/outputs.tf
@@ -0,0 +1,62 @@
+# ------------------------------------------------------------------------------
+# DB EC2
+# ------------------------------------------------------------------------------
+
+output "db_primary_instance_id" {
+  description = "MySQL Primary EC2 Instance ID"
+  value       = aws_instance.db_primary[0].id
+}
+
+output "db_primary_private_ip" {
+  description = "MySQL Primary Private IP"
+  value       = aws_instance.db_primary[0].private_ip
+}
+
+output "db_replica_instance_ids" {
+  description = "MySQL Read Replica EC2 Instance ID 목록"
+  value       = aws_instance.db_replica[*].id
+}
+
+output "db_replica_private_ips" {
+  description = "MySQL Read Replica Private IP 목록"
+  value       = aws_instance.db_replica[*].private_ip
+}
+
+output "db_arbiter_instance_id" {
+  description = "MySQL Arbiter EC2 Instance ID"
+  value       = aws_instance.db_arbiter[0].id
+}
+
+output "db_arbiter_private_ip" {
+  description = "MySQL Arbiter Private IP"
+  value       = aws_instance.db_arbiter[0].private_ip
+}
+
+output "db_sg_id" {
+  description = "DB EC2에 적용된 보안 그룹 ID (modules/security의 db SG)"
+  value       = var.application_sg_id
+}
+
+# ------------------------------------------------------------------------------
+# S3
+# ------------------------------------------------------------------------------
+
+output "fe_bucket_name" {
+  description = "FE/업로드용 S3 버킷 이름"
+  value       = aws_s3_bucket.fe.id
+}
+
+output "fe_bucket_arn" {
+  description = "FE/업로드용 S3 버킷 ARN"
+  value       = aws_s3_bucket.fe.arn
+}
+
+output "backup_bucket_name" {
+  description = "DB 백업용 S3 버킷 이름"
+  value       = aws_s3_bucket.backup.id
+}
+
+output "backup_bucket_arn" {
+  description = "DB 백업용 S3 버킷 ARN"
+  value       = aws_s3_bucket.backup.arn
+}
---
+++ modules/storage/variables.tf
@@ -0,0 +1,98 @@
+# ------------------------------------------------------------------------------
+# 공통 (root에서 전달)
+# ------------------------------------------------------------------------------
+
+variable "environment" {
+  description = "환경 (root variables.tf와 동일)"
+  type        = string
+}
+
+variable "project" {
+  description = "프로젝트 이름 (root variables.tf와 동일)"
+  type        = string
+}
+
+# ------------------------------------------------------------------------------
+# DB EC2 (MySQL Primary / Read Replica / Arbiter)
+# ------------------------------------------------------------------------------
+
+variable "private_db_subnet_ids" {
+  description = "Private DB 서브넷 ID 목록 [subnet_a, subnet_b]"
+  type        = list(string)
+}
+
+variable "application_sg_id" {
+  description = "DB EC2 보안 그룹 ID (modules/security의 security_group_ids.db 전달)"
+  type        = string
+}
+
+variable "db_ami_id" {
+  description = "DB EC2 AMI ID"
+  type        = string
+}
+
+variable "db_key_name" {
+  description = "DB EC2 SSH 키 페어 이름"
+  type        = string
+}
+
+variable "db_primary_instance_type" {
+  description = "MySQL Primary 인스턴스 타입"
+  type        = string
+  default     = "t4g.small"
+}
+
+variable "db_replica_instance_type" {
+  description = "MySQL Read Replica 인스턴스 타입"
+  type        = string
+  default     = "t4g.small"
+}
+
+variable "db_arbiter_instance_type" {
+  description = "Arbiter 인스턴스 타입"
+  type        = string
+  default     = "t4g.nano"
+}
+
+# ------------------------------------------------------------------------------
+# EBS
+# ------------------------------------------------------------------------------
+
+variable "db_root_volume_size_gb" {
+  description = "DB EC2 Root(OS) 볼륨 크기(GB)"
+  type        = number
+  default     = 10
+}
+
+variable "db_data_volume_size_gb" {
+  description = "DB EC2 데이터 전용 EBS 볼륨 크기(GB)"
+  type        = number
+  default     = 20
+}
+
+variable "db_volume_type" {
+  description = "DB EBS 볼륨 타입"
+  type        = string
+  default     = "gp3"
+}
+
+# ------------------------------------------------------------------------------
+# S3
+# ------------------------------------------------------------------------------
+
+variable "fe_bucket_name" {
+  description = "FE 정적 파일 업로드용 S3 버킷 이름"
+  type        = string
+  default     = "planit-v2-fe-s3-bucket"
+}
+
+variable "backup_bucket_name" {
+  description = "DB 백업용 S3 버킷 이름"
+  type        = string
+  default     = "planit-db-backup"
+}
+
+variable "cloudfront_oai_iam_arn" {
+  description = "CloudFront OAI IAM ARN (FE 버킷 GetObject 허용)"
+  type        = string
+}
